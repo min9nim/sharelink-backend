3fcfb8be20e2b64877a3238ebeb5f910de68fa93
songmingu(mac) <min9nim@gmail.com>
2018-10-27 01:30:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sharelink-backend
+sharelink 백엔드 서버
+
+#### 기술스택
+json-server
---
+++ server.js
@@ -0,0 +1,29 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+
+// server.use((req, res, next) => {
+//   if (req.method === 'POST') {
+//     req.body.createdAt = Date.now()
+//   }
+//   // Continue to JSON Server router
+//   next()
+// })
+
+
+server.use(router)
+let port = 80;
+server.listen(port, () => {
+  console.log(`JSON Server is running, port(${port})`)
+})
