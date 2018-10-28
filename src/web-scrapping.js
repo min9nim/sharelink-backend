27ac3ac9bd527c6784460e097fe36a6b7ae4105d
songmingu(mac) <min9nim@gmail.com>
2018-10-29 01:26:54 +0900

    글 desc 가져오기 추가

--- src/web-scrapping.js
+++ src/web-scrapping.js
@@ -6,12 +6,29 @@ const getTitle = async (url) => {
     request(url, function (error, response, body) {
       if (!error) {
         let $ = cheerio.load(body);
-        //let title = $("head title").text();
+
+        // 글제목
         let title = $("meta[property='og:title']").attr("content");
+        if(!title){
+          title = $("head title").text();
+        }
+        
+        // 글이미지
         let image = $("meta[property='og:image']").attr("content");
+        if(!image){
+          image = $("img").attr("src");
+        }
+
+        // 글요약본
+        let desc = $("meta[property='og:description']").attr("content");
+        if(!desc){
+          desc = "";
+        }
+
         console.log("title = " + title);
         console.log("image = " + image);
-        resolve({ url, title, image });
+        console.log("desc = " + desc);
+        resolve({ url, title, image, desc });
       } else {
         console.log("We’ve encountered an error: " + error);
         reject(error);
